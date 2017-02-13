140d2aa84d163da0538a8af1f14b7f1edd6ce2cd
Boris <boris.dorofeev@gmail.com>
2017-02-13 13:38:55 +0600

    Забыл тэг

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,8 +1,8 @@
 <?php
 
-//Route::group(['middleware' => 'auth', 'prefix' => 'adm'], function()
-//{
+Route::group(['middleware' => 'auth', 'prefix' => 'adm'], function()
+{
     Route::get('/file/test',    ['as' => 'clean_file', 'uses' => 'Interpro\FileAggr\Http\FileOperationController@testpage']);
     Route::get('/file/clean',   ['as' => 'clean_file',  'uses' => 'Interpro\FileAggr\Http\FileOperationController@clean']);
     Route::post('/file/upload', ['as' => 'upload_file', 'uses' => 'Interpro\FileAggr\Http\FileOperationController@upload']);
-//});
+});
